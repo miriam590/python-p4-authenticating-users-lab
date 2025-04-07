e2a4a58cec50b005586aaed1b728a7be0fa07b09
miriam590 <miriam590@gmail.com>
2025-04-07 18:34:09 +0300

    done

--- server/app.py
+++ server/app.py
@@ -52,6 +52,31 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+    def post(self):
+        username = request.json.get('username')
+        user = User.query.filter_by(username=username).first()
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {}, 401
+
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = db.session.get(User, user_id)
+            return user.to_dict(), 200
+        return {}, 401
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
